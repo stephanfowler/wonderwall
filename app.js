7d48ea4c4fe17e807c121216774ff7fabbfbbf5d
Stephan Fowler <stephanfowler@gmail.com>
2016-06-09 12:13:45 +0100

    stuff

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 (function() {
 
     var paidPortion = 0.25,
+        feePence = 19,
+        feeMax = 100,
         articleBodyClass = '.js-article__body',
         articleBody  = document.querySelector(articleBodyClass);
 
-    if(!articleBody) { return; }
+    if (window.location.pathname === "/uk/business") {
+        window.localStorage.removeItem("wonderwall");
+    }
 
-    var oldClassName = articleBody.className,
+    if (!articleBody) { return; }
+
+    var tally = parseInt(window.localStorage.getItem("wonderwall") || 0),
+        oldClassName = articleBody.className,
         availableParas = (document.querySelectorAll(articleBodyClass + ' > p') || []).length,
         paidParas = Math.floor(availableParas * paidPortion),
         availableChildren = (document.querySelectorAll(articleBodyClass + ' > *') || []).length,
@@ -21,10 +28,10 @@
                     '<div class="untruncator__cta">&raquo;</div>' +
                     '<b>Finish reading ...</b><br/>' +
                     '<div class="untruncator__desc">' +
-                        '<b>The first two-thirds of an article is ALWAYS FREE</b>. <br>' + 
-                        'Read the rest for only 15p. <br/>' +
-                        'You\'ll never be charged more than £4 per week. ' +
-                        'You\'ve spent £1.65 so far.' +
+                        '<b>The first two-thirds of an article are ALWAYS FREE</b>. <br>' + 
+                        'Read the rest for only ' + feePence + 'p. <br/>' +
+                        'You\'ll never be charged more than £' + feeMax/100  + ' per week. ' +
+                        (tally > 0 && tally < feeMax ? 'You\'ve spent £' + (tally/100)  + ' so far.' : '') +
                     '</div>' +
                 '</div>' +
             '</div>',
@@ -40,6 +47,7 @@
             ".untruncator__desc {font-size: 0.85rem; line-height: 1rem;}" + 
             ".untruncator__cta {float: right; font-size: 100px; margin: 15px 0 0 0; transform: rotate(90deg); opacity: 0.5;}" +
 
+            ".element--supporting {display:none}" + 
             ".element-rich-link {display:none}"; 
 
 
@@ -63,6 +71,8 @@
         setTimeout(function() {
             articleBody.className = oldClassName;
         }, 400);
+
+        window.localStorage.setItem("wonderwall", Math.min(feeMax, tally + feePence))
     }
 
     if (paidParas) {
